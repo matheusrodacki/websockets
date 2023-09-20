df8691eda7f7ee5c5b181e4ec8a714a248cdfb57
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-20 12:47:58 -0300

    Add socket.io dependency and initialize the server with express and socket.io.
    - Add socket.io dependency to package.json
    - Import and initialize socket.io server in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
+//inicializando o express
 const app = express();
 const porta = process.env.porta || 3000;
-
 const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico));
 
-app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
+//inicinado o servidor http
+const servidorHttp = http.createServer(app);
+servidorHttp.listen(porta, () =>
+  console.log(`Servidor rodando na porta ${porta}`)
+);
+
+//iniciando o socket.io
+const io = new Server(servidorHttp);
+io.on("connection", () => {
+  console.log("Um cliente se conectou!");
+});
