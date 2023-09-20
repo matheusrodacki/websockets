31d7a4551c36731450a68b6f883b16165ae182df
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-20 18:40:55 -0300

    feat: Add document selection feature
    - The code now allows users to select a specific document.
    - The selected document's name is displayed in the title.
    - The document's name is passed to the socket when emitting text changes.

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -2,12 +2,21 @@ import { atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
-function emitirTextoEditor(texto) {
-  socket.emit("texto_editor", texto);
+function selecionarDocumento(nome) {
+  socket.emit("selecionar_documento", nome);
+}
+
+function emitirTextoEditor(texto, nomeDocumento) {
+  socket.emit("texto_editor", texto, nomeDocumento);
 }
 
 socket.on("texto_editor_clientes", (texto) => {
   atualizaTextoEditor(texto);
 });
 
-export { emitirTextoEditor };
+socket.on("disconnect", (motivo) => {
+  console.log(`Servidor desconectado!
+  Motivo: ${motivo}`);
+});
+
+export { emitirTextoEditor, selecionarDocumento };
--- src/socket-back.js
+++ src/socket-back.js
@@ -3,7 +3,17 @@ import io from "./index.js";
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
 
-  socket.on("texto_editor", (texto) => {
-    socket.broadcast.emit("texto_editor_clientes", texto);
+  socket.on("selecionar_documento", (nomeDocumento) => {
+    socket.join(nomeDocumento);
+  });
+
+  socket.on("texto_editor", (texto, nomeDocumento) => {
+    //socket.broadcast.emit("texto_editor_clientes", texto);
+    socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+  });
+
+  socket.on("disconnect", (motivo) => {
+    console.log(`Cliente "${socket.id}" desconectado!
+    Motivo: ${motivo}`);
   });
 });
